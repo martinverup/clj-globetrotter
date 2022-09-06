d51659171a4a186605d092fa758930f17e1d42fe
Martin Alexander Z Verup <verup@protonmail.com>
2022-09-06 14:34:20 +0200

    Add functions for displaying cities

--- src/clj_globetrotter/web.cljs
+++ src/clj_globetrotter/web.cljs
@@ -1,6 +1,16 @@
 (ns clj-globetrotter.web
   (:require react-dom
-            [clj-globetrotter.core :as core]))
+            [clojure.string :as str]
+            [clj-globetrotter.core :as core]
+            [clj-globetrotter.world :refer [world]]))
+
+(defonce cities
+  (apply sorted-set (map :source world)))
+
+(defn keyword->name [s]
+  (->> (str/split (name s) #"-")
+       (map str/capitalize)
+       (str/join " ")))
 
 (.render js/ReactDOM
   (.createElement js/React "h2" nil "Hello, React!")
